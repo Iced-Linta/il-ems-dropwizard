ec2c4621bb2ef524cfe0d0e3b8a5f18fa2a3bba7
Dan Adams <daniel.adams@kainos.com>
2024-11-06 16:42:16 +0000

    ILE-3 fixed requested changes

--- src/main/java/org/example/TestApplication.java
+++ src/main/java/org/example/TestApplication.java
@@ -9,8 +9,10 @@ import org.example.controllers.SalesEmployeeController;
 import org.example.controllers.DeliveryEmployeeController;
 import org.example.controllers.TestController;
 import org.example.daos.DeliveryEmployeeDao;
+import org.example.daos.SalesEmployeeDao;
 import org.example.daos.TestDao;
 import org.example.services.DeliveryEmployeeService;
+import org.example.services.SalesEmployeeService;
 import org.example.services.TestService;
 import org.example.validators.DeliveryEmployeeValidator;
 
